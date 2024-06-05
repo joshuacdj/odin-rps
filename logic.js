7d169594e91bc8f8cbab0cac2c97518733245ee3
joshuacdj <joshuacheungdj@gmail.com>
2024-06-05 09:19:00 +0800

    Added the getHumanChoice function to take in user input of either rock, paper or scissors

--- logic.js
+++ logic.js
@@ -1,10 +1,27 @@
 
+const choices = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
 
     let index = Math.floor(Math.random() * 3);
 
-    const choices = ["rock", "paper", "scissors"];
-
     return choices[index];
+}
 
+function getHumanChoice() {
+
+    let choice = prompt("Input a choice (rock, paper, scissors):");
+
+    choice = choice.toLowerCase();
+
+    while (!choices.includes(choice)) {
+
+        console.log("Please input either 'rock', 'paper' or 'scissors' only");
+
+        choice = prompt("Input a choice (rock, paper, scissors):");
+        
+    }
+
+    return choice;
+    
 }
